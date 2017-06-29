191ce34cbb62c99eea741e6d8412f8e06e61b8e5
Chao Charlie Huang <chaocharliehuang@gmail.com>
2017-06-28 18:28:00 -0700

    clicking on a House banner gives that House's info

--- main.js
+++ main.js
@@ -1,10 +1,27 @@
-$("img").click(function(e) {
-    var url = "http://api.openweathermap.org/data/2.5/weather?q=" + cityInput;
-    url += "&&appid=bcd49dc0a841151f291b4e69279dfb71";
+$.ajax({
+      type: "POST",
+      dataType: 'jsonp',
+      success: function(response) {
+        alert("success");
+      },
+      error: function(response) {
+        console.log(JSON.stringify(response));
+      }
+    });
+
+$(".houses img").click(function() {
+    var houseID = $(this).attr("id");
+    var url = "https://www.anapioficeandfire.com/api/houses/" + houseID;
     $.get(url, function(res) {
-        
-        $('.weather-data').html(infoHTML);
+        console.log(res);
+        var infoHTML = "<div class='info col-md-6'>";
+        infoHTML += "<h3>House Details</h3>";
+        infoHTML += "<p><b>Name:</b> " + res.name + "</p>";
+        infoHTML += "<p><b>Words:</b> " + res.words + "</p>";
+        infoHTML += "<p><b>Titles:</b> " + res.titles.join(', ') + "</p>";
+        infoHTML += "</div>";
+        $('.house-info').html(infoHTML);
+        $('.house-info').hide();
+        $('.house-info').fadeIn('slow');
     }, 'json');
 });
-
-// 
